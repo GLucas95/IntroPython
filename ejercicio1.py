53968b0fa35b4b3b762a6ec5e66a9722651fa0a0
GLucas95 <lucasg_95_18@hotmail.com>
2022-07-04 21:49:09 -0300

    commit on main

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,1 +1,24 @@
-print ("hola mundo")
+lista = []
+
+for i in range(0,5):
+    lista.append(int(input("Ingrese un número entero cinco veces:")))
+
+print("Su lista está compuesta por estos cinco números:", lista)    
+
+suma = sum(lista)
+
+print("La suma total de todos los elementos es:",suma)
+
+promedio = suma/len(lista)
+
+print ("El promedio de los elementos es:",promedio)
+
+maxnum = max(lista)
+
+print ("El valor máximo de todos los elementos que contiene la lista es:",maxnum)
+
+minnum = min(lista)
+
+print ("El valor mínimo de todos los elementos que contiene la lista es:",minnum)
+
+print("¡Qué tenga un buen día!")
